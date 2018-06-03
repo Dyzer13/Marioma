f7bb97b6af2dcf837dc9e46f5a13ede035091387
Dyzer13 <39059329+Dyzer13@users.noreply.github.com>
2018-06-03 18:58:14 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const prefix = '.'
-
+bot.setGame('Minecraft')
 client.login('NDQ1OTE1MDYyODA5NTI2Mjgz.Deo9kA.chDGLD3yVwY4Ia-42V6-8m0M6ME');
 
 
